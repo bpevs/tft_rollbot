2b11080d755db8b8045f14e48d6e609334bcc3ef
ivebencrazy <github@bpev.me>
2022-01-15 17:37:37 -0800

    Update basic server code

--- source/deps.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This file organizes and re-exports external dependencies
- * used by Discord Server
- */
-
-export * from "https://raw.githubusercontent.com/discordeno/discordeno/main/mod.ts";
--- source/discord-server.js
+++ source/discord-server.js
@@ -4,30 +4,43 @@
  * @references
  * https://github.com/discordeno/serverless-deno-deploy-template
  * https://discordeno.mod.land/gettingstarted.html
+ * https://github.com/discordeno/template/tree/main/beginner
  */
 
-import { roll } from "./utilities/roll.ts";
+import { startBot } from "https://deno.land/x/discordeno@12.0.1/mod.ts";
+
 import { formatOutputString } from "./utilities/formatOutputString.ts";
-import { ChannelTypes, logGreen } from "./deps.ts";
+import { roll } from "./utilities/roll.ts";
 
-// Setup Roll Command
-botCache.command.set("roll", {
-  callback: (message) => {
-    const rolls = roll(new Set(["player1"]));
+// Ripped from examples, but Code doesn't work idk why.
+startBot({
+  token: "my-token-here",
+  intents: ["GUILDS", "GUILD_MESSAGES"],
+  eventHandlers: {
+    ready() {
+      console.log("Successfully connected to gateway");
+    },
+    messageCreate(message) {
+      if (message.content === "!roll") {
+        // parse message for player names here?
 
-    let text = "TFT RollBot (Season 6):\n\n";
+        const rolls = roll(new Set(["player1"]));
 
-    Object.keys(rolls).forEach((playerName) => {
-      const roll = rolls[playerName];
-      const { type, origin, king } = roll;
+        let text = "TFT RollBot (Season 6):\n\n";
 
-      text += (
-        (numPlayers > 1 ? `${playerName}\n` : "") +
-        `  King: ${king}\n` +
-        `  Team: ${type} + ${origin}\n\n`
-      );
-    });
+        Object.keys(rolls).forEach((playerName) => {
+          const roll = rolls[playerName];
+          const { type, origin, king } = roll;
 
-    return message.channel.sendMessage(text);
+          text += (
+            (numPlayers > 1 ? `${playerName}\n` : "") +
+            `  King: ${king}\n` +
+            `  Team: ${type} + ${origin}\n\n`
+          );
+        });
+
+        message.reply(text);
+      }
+    },
   },
 });
