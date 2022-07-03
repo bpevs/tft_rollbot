98f44272e962bfc2b05f50c68c1342a225a7530c
Ben Pevsner <git@bpev.me>
2022-07-03 16:03:43 -0700

    feat: basic bot

--- source/discord-server.ts
+++ source/discord-server.ts
@@ -1,56 +1,93 @@
-import "dotenv";
+// Sift is a small routing library that abstracts away details like starting a
+// listener on a port, and provides a simple function (serve) that has an API
+// to invoke a function for a specific path.
 import {
-  ApplicationCommandInteraction,
-  Client,
-  event,
-  Intents,
-  slash,
-  SlashCommandOptionType,
-  SlashCommandPartial,
-} from "harmony";
-import { ENVIRONMENT } from "./constants.ts";
-import { formatOutputString } from "./utilities/formatOutputString.ts";
-import { roll } from "./utilities/roll.ts";
+  json,
+  serve,
+  validateRequest,
+} from "https://deno.land/x/sift/mod.ts";
+// TweetNaCl is a cryptography library that we use to verify requests
+// from Discord.
+import nacl from "https://cdn.skypack.dev/tweetnacl@v1.0.3?dts";
 
-const commands: SlashCommandPartial[] = [
-  {
-    name: "roll",
-    description: "Roll a TFT Comp",
-    options: [
+// For all requests to "/" endpoint, we want to invoke home() handler.
+serve({
+  "/": home,
+});
+
+// The main logic of the Discord Slash Command is defined in this function.
+async function home(request: Request) {
+  // validateRequest() ensures that a request is of POST method and
+  // has the following headers.
+  const { error } = await validateRequest(request, {
+    POST: {
+      headers: ["X-Signature-Ed25519", "X-Signature-Timestamp"],
+    },
+  });
+  if (error) {
+    return json({ error: error.message }, { status: error.status });
+  }
+
+  // verifySignature() verifies if the request is coming from Discord.
+  // When the request's signature is not valid, we return a 401 and this is
+  // important as Discord sends invalid requests to test our verification.
+  const { valid, body } = await verifySignature(request);
+  if (!valid) {
+    return json(
+      { error: "Invalid request" },
       {
-        name: "names",
-        description: "Add player names to avoid overlapping comps",
-        required: false,
-        type: SlashCommandOptionType.STRING,
+        status: 401,
       },
-    ],
-  },
-];
+    );
+  }
 
-class TagBot extends Client {
-  @event()
-  ready() {
-    console.log("Ready!");
-    commands.forEach((command) => {
-      this.slash.commands.create(command)
-        .then((cmd) => console.log(`Created Slash Command ${cmd.name}!`))
-        .catch((err) => console.log(`Failed to create command!`, err));
+  const { type = 0, data = { options: [] } } = JSON.parse(body);
+  // Discord performs Ping interactions to test our application.
+  // Type 1 in a request implies a Ping interaction.
+  if (type === 1) {
+    return json({
+      type: 1, // Type 1 in a response is a Pong interaction response type.
     });
   }
 
-  @slash()
-  roll(i: ApplicationCommandInteraction) {
-    const options = i?.options;
-    const players = (options[0]?.value || "").split(" ").filter(Boolean);
-    const tooManyPlayers = players.length > 8;
-    if (tooManyPlayers) i.respond({ content: "Too many players!" });
-
-    // Default to rolling for 1 player
-    const rolls = roll(new Set(players.length ? players : ["default"]));
-    const content = formatOutputString(rolls, ENVIRONMENT.DISCORD);
-    i.respond({ content });
+  // Type 2 in a request is an ApplicationCommand interaction.
+  // It implies that a user has issued a command.
+  if (type === 2) {
+    const { value } = data.options.find((option) => option.name === "name");
+    return json({
+      // Type 4 responds with the below message retaining the user's
+      // input at the top.
+      type: 4,
+      data: {
+        content: `Hello, ${value}!`,
+      },
+    });
   }
+
+  // We will return a bad request error as a valid Discord request
+  // shouldn't reach here.
+  return json({ error: "bad request" }, { status: 400 });
 }
 
-const bot = new TagBot();
-bot.connect(Deno.env.get("DISCORD_TOKEN"), Intents.None);
+/** Verify whether the request is coming from Discord. */
+async function verifySignature(
+  request: Request,
+): Promise<{ valid: boolean; body: string }> {
+  const PUBLIC_KEY = Deno.env.get("DISCORD_PUBLIC_KEY")!;
+  // Discord sends these headers with every request.
+  const signature = request.headers.get("X-Signature-Ed25519")!;
+  const timestamp = request.headers.get("X-Signature-Timestamp")!;
+  const body = await request.text();
+  const valid = nacl.sign.detached.verify(
+    new TextEncoder().encode(timestamp + body),
+    hexToUint8Array(signature),
+    hexToUint8Array(PUBLIC_KEY),
+  );
+
+  return { valid, body };
+}
+
+/** Converts a hexadecimal string to Uint8Array. */
+function hexToUint8Array(hex: string) {
+  return new Uint8Array(hex.match(/.{1,2}/g)!.map((val) => parseInt(val, 16)));
+}
